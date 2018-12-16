aa87f29edd555636edb1c03109b607df2f80dbb4
Anand A Joshi <ajoshi@masal.usc.edu>
2018-12-15 16:13:03 -0800

    update

---
+++ reg_prepost/reg_prepost.m
@@ -0,0 +1,48 @@
+
+function reg_prepost(bse_exe, mov_img, ref_img, reg_img, err_file)
+
+mov_img_bse=[tempname(),'.nii.gz'];
+ref_img_bse=[tempname(),'.nii.gz'];
+outdir = tempname();
+
+%zip the nii files, as the codes that are used later need .nii.gz
+%extensions
+
+gzip(mov_img,outdir);
+[pth, fname]=fileparts(mov_img);
+mov_img=fullfile(out_dir,fname,'nii.gz');
+gzip(ref_img,outdir);
+
+ref_img=[ref_img,'.gz'];
+[pth, fname]=fileparts(ref_img);
+ref_img=fullfile(out_dir,fname,'nii.gz');
+
+similarity='cr';
+%perform skull stripping used for later
+system(sprintf('%s -i %s -o %s --auto --trim',bse_exe, mov_img, mov_img_bse));
+
+%perform rigid registration
+usc_rigid_reg(mov_img, ref_img, reg_img, similarity, [])
+
+% Load the images and normalize their intensities
+vref = load_untouch_nii_gz(ref_img);
+vwrp = load_untouch_nii_gz(reg_img);
+msk = load_untouch_nii_gz(ref_img_bse);
+vwrp.img=(255.0/max(double(vwrp.img(msk.img(:)>0))))*double(vwrp.img);
+vref.img=(255.0/max(double(vref.img(msk.img(:)>0))))*double(vref.img);
+
+% compute the error and smooth the error
+vwrp.img = sqrt((double(vref.img) - double(vwrp.img)).^2);
+vwrp.img = vwrp.img.*(msk.img>0);
+vwrp.img=smooth3(vwrp.img,'gaussian',[7,7,7],1);
+
+% Try to estimate the error mask using morphological operations
+vwrp.img=bwareaopen(255.0*(vwrp.img>30),50,18);
+
+SE = strel('cube',3);
+vwrp.img=imerode(vwrp.img,SE);
+vwrp.img=bwareaopen(255.0*(vwrp.img>0),150,18);
+vwrp.img=imdilate(vwrp.img,SE);
+
+save_untouch_nii_gz(vwrp, err_file, 64);
+
