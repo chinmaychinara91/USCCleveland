e6df0cd2f93b6084eee6055ff3454187e94d42b5
ajoshi <ajoshi@erdos.usc.edu>
2020-10-25 03:44:13 -0700

    update

--- reg_prepost/sample_rigid_reg_post2pre.m
+++ reg_prepost/sample_rigid_reg_post2pre.m
@@ -3,19 +3,25 @@ addpath(genpath('.'));
 
 %% Set the input arguments
 bse_exe = '/home/ajoshi/BrainSuite19b/bin/bse';
+dir={'215_F1979J44','238_F1980J4S'}
+premri={'F1979J44_preMRI','F1980J4S_preMRI'}
+postmri={'F1979J44_postRS_MRI','F1980J4S_postRS_MRI'}
 
-mov_img = '/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/Ken_Post-op_MRI/sub-SUB118/sMRI/sub-SUB118-118_MRI_1.nii';
-ref_img = '/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/Andrew_Pre-op_MRI_and_EZ_Map/Subject118/T1s.nii.gz';
-%% The following two files contain the output file names
-
-% rigidly warped image
-reg_img='/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/Andrew_Pre-op_MRI_and_EZ_Map/Subject118/post2pre.nii.gz';
-
-% estimated error mask indicating resection or ablation
-% This is not reliable, as it is suseptible to MRI artifacts
-%If the error mask is not correct, then open 'ref_img' above in BrainSuite 
-% and the use mask tool from BrainSuite to get the correct mask.
-err_file='/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/Andrew_Pre-op_MRI_and_EZ_Map/Subject118/error.nii.gz';
-
-%perform the registration
-reg_prepost(bse_exe, mov_img,ref_img,reg_img,err_file);
+for j=1:length(dir)
+    mov_img = ['/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/post2prenii/post2pre_10_22_2020/de_identified/',dir{j},'/',postmri{j},'.nii'];
+    ref_img = ['/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/post2prenii/post2pre_10_22_2020/de_identified/',dir{j},'/',premri{j},'.nii'];
+    %% The following two files contain the output file names
+    
+    % rigidly warped image
+    reg_img=['/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/post2prenii/post2pre_10_22_2020/de_identified/',dir{j},'/post2pre.nii.gz'];
+    
+    % estimated error mask indicating resection or ablation
+    % This is not reliable, as it is suseptible to MRI artifacts
+    %If the error mask is not correct, then open 'ref_img' above in BrainSuite
+    % and the use mask tool from BrainSuite to get the correct mask.
+    err_file=['/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/post2prenii/post2pre_10_22_2020/de_identified/',dir{j},'/error.nii.gz'];
+    
+    %perform the registration
+    reg_prepost(bse_exe, mov_img,ref_img,reg_img,err_file);
+    
+end
