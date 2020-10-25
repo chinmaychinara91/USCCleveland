72aff6d4d86b33c270937f2c06842389117d772a
ajoshi <ajoshi@erdos.usc.edu>
2020-10-25 03:51:34 -0700

    update

--- reg_prepost/sample_rigid_reg_post2pre.m
+++ reg_prepost/sample_rigid_reg_post2pre.m
@@ -3,9 +3,9 @@ addpath(genpath('.'));
 
 %% Set the input arguments
 bse_exe = '/home/ajoshi/BrainSuite19b/bin/bse';
-dir={'215_F1979J44','238_F1980J4S'}
-premri={'F1979J44_preMRI','F1980J4S_preMRI'}
-postmri={'F1979J44_postRS_MRI','F1980J4S_postRS_MRI'}
+dir={'215_F1979J44','238_F1980J4S','233_F1989J4P','231_M1979J41_2','231_M1979J41_1','221_F1973J56','219_M1984K53'};
+premri={'F1979J44_preMRI','F1980J4S_preMRI','F1989J4P_preMRI','M1979J41_preMRI','M1979J41_preMRI','F1973J56_preMRI','M1984K53_preMRI'};
+postmri={'F1979J44_postRS_MRI','F1980J4S_postRS_MRI','F1989J4P_postRS_MRI','M1979J41_postRS_MRI_2','M1979J41_postRS_MRI_1','F1973J56_postRS_MRI','M1984K53_postRS_MRI'};
 
 for j=1:length(dir)
     mov_img = ['/ImagePTE1/ajoshi/HBM_Fingerprint_Data_For_Anand/post2prenii/post2pre_10_22_2020/de_identified/',dir{j},'/',postmri{j},'.nii'];
@@ -25,3 +25,5 @@ for j=1:length(dir)
     reg_prepost(bse_exe, mov_img,ref_img,reg_img,err_file);
     
 end
+
+
