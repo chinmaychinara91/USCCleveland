4af5e42878aca48f2a3dcc3ebd3598abd428488b
ajoshi <ajoshi@erdos.usc.edu>
2020-10-25 17:14:07 -0700

    update

--- reg_prepost/sample_rigid_reg_post2pre.m
+++ reg_prepost/sample_rigid_reg_post2pre.m
@@ -4,7 +4,7 @@ addpath(genpath('.'));
 %% Set the input arguments
 bse_exe = '/home/ajoshi/BrainSuite19b/bin/bse';
 dir={'215_F1979J44','238_F1980J4S','233_F1989J4P','231_M1979J41_2','231_M1979J41_1','221_F1973J56','219_M1984K53'};
-premri={'F1979J44_preMRI','F1980J4S_preMRI','F1989J4P_preMRI','M1979J41_preMRI','M1979J41_preMRI','F1973J56_preMRI','M1984K53_preMRI'};
+premri={'F1979J44_preMRI','F1980J4S_preMRI','F1989J4P_preMRI','M1979J41_preMRI','M1979J41_preMRI.bse','F1973J56_preMRI','M1984K53_preMRI'};
 postmri={'F1979J44_postRS_MRI','F1980J4S_postRS_MRI','F1989J4P_postRS_MRI','M1979J41_postRS_MRI_2','M1979J41_postRS_MRI_1','F1973J56_postRS_MRI','M1984K53_postRS_MRI'};
 
 for j=1:length(dir)
