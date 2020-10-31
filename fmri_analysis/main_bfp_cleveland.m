99dd4c81303bd8d4487f17794c7c10e45c44b287
Anand A Joshi <ajoshi@masal.usc.edu>
2020-10-31 16:39:19 -0700

    update

--- fmri_analysis/main_bfp_cleveland.m
+++ fmri_analysis/main_bfp_cleveland.m
@@ -1,10 +1,15 @@
 clc;clear all;close all;restoredefaultpath;
-addpath(genpath('/ImagePTE1/ajoshi/code_farm/bfp/src'));
+%addpath(genpath('/big_disk/ajoshi/coding_ground/bfp/supp_data'))
+%addpath(genpath('/ImagePTE1/ajoshi/code_farm/svreg/dev'));
 
+%addpath(genpath('/ImagePTE1/ajoshi/code_farm/svreg/src'));
+%addpath(genpath('/ImagePTE1/ajoshi/code_farm/svreg/3rdParty'));
+
+addpath(genpath('/ImagePTE1/ajoshi/code_farm/bfp/src'));
 % Set the input arguments
 configfile='/big_disk/ajoshi/for_cleveland/bfpout/config_bfp_preproc.ini';
 
-subname={'study12098','study12028','study11258','study12525','study12554','study13072'};
+subname={'0019003','0019001','0019006','0019005','0019004','0019002','study12098','study12028','study11258','study12525','study12554','study13072'};
 
 for jj=1:length(subname)
     t1=sprintf('/big_disk/ajoshi/for_cleveland/fmri_data/%s_MRI.nii.gz',subname{jj});
@@ -15,6 +20,7 @@ for jj=1:length(subname)
     sessionid='rest';
     
     % Call the bfp function
-    bfp(configfile, t1, fmri, studydir, subid, sessionid, '');
-    
+ %   bfp(configfile, t1, fmri, studydir, subid, sessionid, '');
+    gen_brainordinates('/home/ajoshi/BrainSuite19b', studydir, subid, sessionid);
+ 
 end
