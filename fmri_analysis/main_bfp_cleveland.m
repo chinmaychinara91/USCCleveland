20456d42fc815147bc0216b9bbf89f6a1eee9e5e
Anand A Joshi <ajoshi@masal.usc.edu>
2021-07-01 00:03:45 -0700

    update

--- fmri_analysis/main_bfp_cleveland.m
+++ fmri_analysis/main_bfp_cleveland.m
@@ -9,6 +9,7 @@ addpath(genpath('/ImagePTE1/ajoshi/code_farm/bfp/src'));
 % Set the input arguments
 configfile='/big_disk/ajoshi/for_cleveland/bfpout/config_bfp_preproc_2020.ini';
 
+%subname={'study12098','study12028','study11258','study12525','study12554','study13072','0019003','0019001','0019006','0019005','0019004','0019002'};
 subname={'study12098','study12028','study11258','study12525','study12554','study13072','0019003','0019001','0019006','0019005','0019004','0019002'};
 
 for jj=1:length(subname)
